2d0dc81b0b1e50a2276ebbac45ad0621578db693
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-01-06 23:32:56 +0530

    Day 07: Polymorphism (Overloading & Overriding)

---
+++ day07_polymorphism/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person p = new Student();
+        p.work(); 
+        
+        p.info("Arpan");
+        p.info("Arpan", 21);
+    }
+}
---
+++ day07_polymorphism/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    void work() {
+        System.out.println("Person is working");
+    }
+
+    void info(String name) {
+        System.out.println("Name: " + name);
+    }
+
+    void info(String name, int age) {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
