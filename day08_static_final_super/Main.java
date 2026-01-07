0b543bb8ceb77ab3b62a7ee77d60f75efa9694b9
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-01-07 19:10:49 +0530

    Day 08: static, final, and super keyword

---
+++ day08_static_final_super/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Arpan", 101);
+        Student s2 = new Student("Rahul", 102);
+
+        s1.show();
+        s2.show();
+    }
+}
