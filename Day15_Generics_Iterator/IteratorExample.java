6c9baab978574af7db6bc0d82d6a369420e7bacf
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-01-14 10:12:31 +0530

    Day 15: Generics and Iterator in Java

---
+++ Day15_Generics_Iterator/IteratorExample.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        Iterator<String> itr = list.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
