c6acca2cdbaf3e4676bfa9014515719d3970b50b
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-01-15 10:20:18 +0530

    Day 16: Comparable vs Comparator sorting examples

---
+++ day16_comparable_comparator/SortByMarks.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class SortByMarks implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        return s2.marks - s1.marks; 
+    }
+}
