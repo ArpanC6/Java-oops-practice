9578e13af382e97f984876bba7a9a26e71cfd5c1
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-01-30 09:45:17 +0530

    Day 31: Implemented Singlentonand Factory design patterns

---
+++ day31_design_patterns/factory/FactoryMain.java
@@ -0,0 +1,9 @@
+public class FactoryMain {
+    public static void main(String[] args) {
+
+        Notification notification =
+                NotificationFactory.createNotification("EMAIL");
+
+        notification.notifyUser();
+    }
+}
