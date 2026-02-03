4d0972a55d84d299059f1f597b9389441684b370
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-02-03 10:50:02 +0530

    Day 35: Implemented Java multithreading concepts with synchronization examples

---
+++ day35_multithreading/ThreadCreationDemo.java
@@ -0,0 +1,11 @@
+class MyThread extends Thread {
+
+    public void run() {
+        System.out.println("Thread running: " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();   // NOT run()
+    }
+}
