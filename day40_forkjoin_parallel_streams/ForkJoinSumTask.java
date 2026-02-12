50c4d0987ebb5d0fa7fca25cf307f72ce0dcd270
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-02-12 09:43:11 +0530

    Day 40: ForkJoin framework and parrallel streams

---
+++ day40_forkjoin_parallel_streams/ForkJoinSumTask.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ForkJoinPool;
+
+class SumTask extends RecursiveTask<Long> {
+
+    private final int[] numbers;
+    private final int start;
+    private final int end;
+    private static final int THRESHOLD = 5;
+
+    SumTask(int[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        if (end - start <= THRESHOLD) {
+            long sum = 0;
+            for (int i = start; i < end; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        int mid = (start + end) / 2;
+
+        SumTask leftTask = new SumTask(numbers, start, mid);
+        SumTask rightTask = new SumTask(numbers, mid, end);
+
+        leftTask.fork();              // parallel start
+        long rightResult = rightTask.compute();
+        long leftResult = leftTask.join();
+
+        return leftResult + rightResult;
+    }
+}
+
+public class ForkJoinSumTask {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+        }
+
+        ForkJoinPool pool = new ForkJoinPool();
+        long result = pool.invoke(new SumTask(numbers, 0, numbers.length));
+
+        System.out.println("Total Sum: " + result);
+    }
+}
