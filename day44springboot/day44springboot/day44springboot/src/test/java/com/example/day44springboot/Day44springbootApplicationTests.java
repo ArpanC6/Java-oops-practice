0f905127bf72b2f2f0e598b36ed6c70bbc48807d
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-02-16 10:51:05 +0530

    Day 44 update

---
+++ day44springboot/day44springboot/day44springboot/src/test/java/com/example/day44springboot/Day44springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day44springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day44springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
