31cfe345b03a9a2c3358a72accb7ca5f6a1d5313
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-02-19 09:10:09 +0530

    Day 47: Spring Boot project setup with IntelliJ

---
+++ day47springboot/src/main/java/com/example/day47springboot/Day47springbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.day47springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day47springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day47springbootApplication.class, args);
+	}
+
+}
---
+++ day47springboot/src/test/java/com/example/day47springboot/Day47springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day47springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day47springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
